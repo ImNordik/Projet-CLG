923d34463e2058f1249de2eb039cb9cb2f585b97
ImNordik <alexandre.landreville@gmail.com>
2016-02-01 13:31:00 -0500

    SUP!
    Un ptit message pour vous les boys

---
+++ whatup boys/whatup boys/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatup_boys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SUP!");
+        }
+    }
+}
